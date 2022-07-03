658642edee1f7b546a7dd21611ee29e4581faebc
Ennia-Fahd <92646945+Ennia-Fahd@users.noreply.github.com>
2022-07-03 16:01:26 +0100

    Update 0001_initial.py

--- home/migrations/0001_initial.py
+++ home/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     dependencies = [
     ]
 
-    operations = [
+    operations = [ 
         migrations.CreateModel(
             name='SliderContent',
             fields=[
